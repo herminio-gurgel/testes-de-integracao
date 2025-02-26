feed86c109720339e501237d5304d5e328ca9580
Hermíno Carlos Gurgel Medeiros Netto <herminio.gurgel@gmail.com>
2025-02-26 12:15:16 -0300

    2.4 Trabalhando com transações

--- tests/Integration/Dao/LeilaoDaoTest.php
+++ tests/Integration/Dao/LeilaoDaoTest.php
@@ -14,6 +14,7 @@ class LeilaoDaoTest extends TestCase
     protected function setUp(): void
     {
         $this->pdo = ConnectionCreator::getConnection();
+        $this->pdo->beginTransaction();
     }
 
     public function testInsercaoEBuscaDevemFuncionar()
@@ -34,7 +35,7 @@ class LeilaoDaoTest extends TestCase
 
     protected function tearDown(): void
     {
-        $this->pdo->exec('DELETE FROM leiloes');
+        $this->pdo->rollBack();
     }
 }
 
@@ -45,4 +46,10 @@ class LeilaoDaoTest extends TestCase
  * 2.2 Removendo após o teste
  * Usamos a função tearDown() para deletar todos os dados do banco e poder normalizar os testes.
  * Contudo, apagar todos os dados do banco não é recomendado.
+ *
+ * 2.4 Trabalhando com transações
+ * Usando as funções beginTransaction() e roolback() dentro dos métodos setUp e tearDown, resolve o problema de alterar
+ * a base de dados sem necessidade.
+ * Porém, em escala isso continua sendo um problema, bancos muitos populados ou com baixa eficiência podem causar
+ * lentidão nos testes, então é preciso uma solução que use um banco de dados, mas que não seja o mesmo de produção.
  */
